69f6fce7878bf2f99cb35091e0018b557be6f800
Estevan Cantuaria <estevancantuaria@MacBook-Air-de-Estevan.local>
2025-06-09 15:04:19 -0300

    feat: adding subnets

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.0.0-beta3"
     }
+  }
+  backend "s3" {
+    bucket = "estevanlabs"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
