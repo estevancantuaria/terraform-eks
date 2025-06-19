91b8e34f8f95525687da46e6cd670af8760145f1
Estevan Cantuaria <estevancantuaria@MacBook-Air-de-Estevan.local>
2025-06-19 11:25:19 -0300

    feat: eks module call

---
+++ modules/cluster/variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+  type        = string
+  description = "Project name to be used to name the resources (Name tag)"
+}
+
+variable "public_subnet_1a" {
+  type        = string
+  description = "Public subnet 1a"
+}
+
+variable "public_subnet_1b" {
+  type        = string
+  description = "Public subnet 1b"
+}
--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
-output "vpc_id" {
-  value = module.eks_network.vpc_id
+output "oidc" {
+  value = module.eks_cluster.oidc
 }
